9a1cbffecf0400dc3f0c45b977abc31e1ed55d7a
khknopp <kajetan@knopp.com.pl>
2023-10-01 03:44:44 +0200

    Added better circuits

--- quantum.py
+++ quantum.py
@@ -1,5 +1,6 @@
 from qiskit import transpile, QuantumCircuit
 from qiskit_aer import AerSimulator
+import random
 
 
 backend = AerSimulator()
@@ -10,11 +11,34 @@ sounds = {'0000': 'C', '0001': 'Cis', '0010': 'D',  '0011': 'Dis', '0100': 'E',
 
 special = ['/', '*']
 
+def apply_random_gate(qc, qubit, other_qubits):
+    gate_choice = random.choice(['H', 'X', 'Y', 'Z', 'CX', 'RY', 'RZ'])
 
-def generate_circuit():
-    circ = QuantumCircuit(4, 4)
-    circ.h([0, 1, 2, 3])
-    return circ
+    if gate_choice == 'H':
+        qc.h(qubit)
+    elif gate_choice == 'X':
+        qc.x(qubit)
+    elif gate_choice == 'Y':
+        qc.y(qubit)
+    elif gate_choice == 'Z':
+        qc.z(qubit)
+    elif gate_choice == 'CX':
+        control = random.choice(other_qubits)
+        qc.cx(control, qubit)
+    elif gate_choice == 'RY':
+        angle = random.uniform(0, 2 * 3.14159)
+        qc.ry(angle, qubit)
+    elif gate_choice == 'RZ':
+        angle = random.uniform(0, 2 * 3.14159)
+        qc.rz(angle, qubit)
+
+def generate_circuit(qc):
+    for _ in range(5):
+        target_qubit = random.choice([0, 1, 2, 3])
+        other_qubits = [q for q in [0, 1, 2, 3] if q != target_qubit]
+        apply_random_gate(qc, target_qubit, other_qubits)
+
+    return qc
 
 
 def get_value(circ):
@@ -32,9 +56,11 @@ def run_tact():
     position = 0
     sounds = []
 
+    qc = QuantumCircuit(4,4)
+
     while (position <= 7):
-        sound_1 = get_value(generate_circuit())
-        sound_2 = get_value(generate_circuit())
+        sound_1 = get_value(generate_circuit(qc))
+        sound_2 = get_value(generate_circuit(qc))
         if (sound_1 in special):
             continue
 
@@ -73,7 +99,3 @@ def create_song(length):
     for i in range(length):
         tacts.append(run_tact())
     return tacts
-
-
-song = create_song(10)
-print(song)
